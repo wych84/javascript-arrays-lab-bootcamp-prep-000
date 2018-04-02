cd0d8bc154866bb609779d33add7f12b864f86a5
wooyangee@gmail.com <wooyangee@gmail.com>
2018-04-02 05:42:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, "Broom"]
+}
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens]
+}
+
+function removeLastKitten() {
+var removeLastKitten = ["Milo", "Otis", "Garfield"]
+removeLastKitten = removeLastKitten.slice(0, removeLastKitten.length -1)
+return removeLastKitten;
+}
+
